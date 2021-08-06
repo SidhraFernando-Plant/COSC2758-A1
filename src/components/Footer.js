07bbb94a41490d386fd7461a5b5e2a9fce1edb7a
“SidhraFernando-Plant <s3873756@student.rmit.edu.au>
2021-08-06 23:57:17 +1000

    First version of footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,15 @@
 function Footer() {
     return (
-      <p>Hello Footer!</p>
+      <div className="bg-light">
+        <hr></hr>
+        <div className="d-flex justify-content-center">
+          <p className="p-3">Ⓒ 2021 Sidhra Fernando-Plant</p>
+          <p className="p-3 text-muted">|</p>
+          <p className="p-3">Sign up</p>
+          <p className="p-3 text-muted">|</p>
+          <p className="p-3">Log in</p>
+        </div>
+      </div>
     );
   }
   
